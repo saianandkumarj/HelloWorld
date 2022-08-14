cbf4ecc8027d9dc8ba535cbd7c0c57e4804cf460
Anand <saianandkumarj@gmail.com>
2022-08-14 19:41:32 +0530

    Adding new proj

---
+++ src/main/java/com/example/sonarqueb/SonarquebApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarqueb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarquebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarquebApplication.class, args);
+	}
+
+}
